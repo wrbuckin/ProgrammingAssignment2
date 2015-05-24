843bf121a0b32171aede0d01fe575c95694ffd37
wrbuckin <Bill@Bills-MacBook-Pro.local>
2015-05-23 22:36:30 -0500

    solve for assignment 2
    solution to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates a matrix that has the ability to cache its own inverse
+## cacheSolve() computes the inverse of the matrix from the makeCacheMatrix function
+## This function checks to see if the inverse has been calcuated. If so, the function
+## retrieves the cached inverse. If it is not present the function calculates the inverse using solve
+## The caching speeds up the calculation  -serving as a demo for big data exercises
 
-## Write a short comment describing this function
+## Create the matrix and cache the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invM <- NULL
+        set <- function(y) {
+                x <<- y ## <<- assigns value to the object in the env outside of the current
+                invM <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inv) invM <<- inverse
+        getInv <- function() invM
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv) # preps the functions for the cache test
 }
 
 
-## Write a short comment describing this function
 
+## compute the inverse from above, check the cache, if present retireve - otherwise complete the calculation
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInv()
+        # function option if the inverse is in the cache
+        if(!is.null(invM)) {
+                # retrieves the inverse from the cache and prevents the computation from running, alerting the user
+                message("getting cached data")
+                return(invM)
+        }
+        # if inverse is not in cache, inverse calculation is run
+        data <- x$get()
+        m <- solve(data, ...)
+        
+        # runs the setInv function to compute the value for the inverse and sets the variable to that value
+        x$setInv(m)
+        return(invM)
 }
+
+
